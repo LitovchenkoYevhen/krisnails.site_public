ce751a07f2946f96f0475d9b2e1be9606a68be8d
Yevhen Litovchenko <litovchenkoyevhen@gmail.com>
2021-07-14 14:28:25 +0300

    add chache

--- services/urls.py
+++ services/urls.py
@@ -9,11 +9,11 @@ from services import views
 app_name = 'services'
 urlpatterns = [
     path('', cache_page(60 * 1)(views.Home.as_view()), name='home'),
-    path('service/', views.Services.as_view(), name='service'),
-    path('portfolio/', views.Portfolio.as_view(), name='portfolio'),
+    path('service/', cache_page(60 * 1)(views.Services.as_view()), name='service'),
+    path('portfolio/', cache_page(60 * 1)(views.Portfolio.as_view()), name='portfolio'),
     path('contact/', views.send_message, name='contact'),
-    path('about/', views.About.as_view(), name='about'),
-    path('blog/', views.Blog.as_view(), name='blog'),
-    path('blog/<str:slug>', views.ShowPost.as_view(), name='show_post'),
+    path('about/', cache_page(60 * 1)(views.About.as_view()), name='about'),
+    path('blog/', cache_page(60 * 1)(views.Blog.as_view()), name='blog'),
+    path('blog/<str:slug>', cache_page(60 * 1)(views.ShowPost.as_view()), name='show_post'),
     path('contact_form/', views.send_message, name='contact_form'),
 ]
