af3bda758a4e84bcb2dc07bfba4101112448aa97
Yevhen Litovchenko <litovchenkoyevhen@gmail.com>
2021-07-15 16:26:27 +0300

    alter button _nav.html

--- services/views.py
+++ services/views.py
@@ -44,8 +44,7 @@ def send_message(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.cleaned_data['subject'] = form.cleaned_data['telephone'] + ' | ' + form.cleaned_data['name'] + ' | ' + form.cleaned_data['email']
-            mail = send_mail(form.cleaned_data['subject'], form.cleaned_data['content'], 'krisnails@ukr.net',
-                             ('litovchenkoyevhen@gmail.com',))
+            mail = send_mail(form.cleaned_data['subject'], form.cleaned_data['content'], 'krisnails@ukr.net', ('litovchenkoyevhen@gmail.com',))
             if mail:
                 messages.success(request, 'Письмо отправлено!')
                 return redirect('services:contact')
